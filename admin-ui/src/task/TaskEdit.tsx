f5ea1517cf1717c0685b2be3d4245875297d74a2
iznow.tech <iznotek@users.noreply.github.com>
2021-07-13 10:01:12 +0200

    amplication-build-ckr1rmdn57606121bs646pb8rl1

---
+++ admin-ui/src/task/TaskEdit.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+
+import {
+  Edit,
+  SimpleForm,
+  EditProps,
+  ReferenceInput,
+  SelectInput,
+  NumberInput,
+  DateInput,
+  TextInput,
+} from "react-admin";
+
+import { UserTitle } from "../user/UserTitle";
+import { ProjectTitle } from "../project/ProjectTitle";
+
+export const TaskEdit = (props: EditProps): React.ReactElement => {
+  return (
+    <Edit {...props}>
+      <SimpleForm>
+        <ReferenceInput source="user.id" reference="User" label="Assigned To">
+          <SelectInput optionText={UserTitle} />
+        </ReferenceInput>
+        <NumberInput step={1} label="Estimation (days)" source="estimation" />
+        <ReferenceInput source="project.id" reference="Project" label="Project">
+          <SelectInput optionText={ProjectTitle} />
+        </ReferenceInput>
+        <DateInput label="Start Date" source="startDate" />
+        <SelectInput
+          source="status"
+          label="Status"
+          choices={[
+            { label: "New", value: "new" },
+            { label: "Pending", value: "pending" },
+            { label: "On Hold", value: "onHold" },
+            { label: "Ongoing", value: "ongoing" },
+            { label: "Done", value: "done" },
+          ]}
+          optionText="label"
+          optionValue="value"
+        />
+        <TextInput label="Title" source="title" />
+      </SimpleForm>
+    </Edit>
+  );
+};
